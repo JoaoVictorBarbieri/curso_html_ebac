470214117ebc3c0865ad716c008697a43fcdafd2
João <joaov.b.euwest@gmail.com>
2024-04-11 12:56:18 -0300

    #gulp-sass-compilado

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass( {
+            outputStyle:'compressed'
+        }))
+        .pipe(sourcemaps.write('.maps'))
+        .pipe(gulp.dest('build/styles'))
+    }
 function funcaoPadrao(callBack) {
     console.log('Executando via gulp')
     callBack();
@@ -13,3 +26,7 @@ function dizTchau() {
 }
 exports.default = funcaoPadrao;
 exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function(){
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+}
