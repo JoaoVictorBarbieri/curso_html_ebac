f941f924eb4664862686b036c019a05f7dd3d605
João <joaov.b.euwest@gmail.com>
2024-04-17 11:27:19 -0300

    #create dev/dist

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,25 @@
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css':'source/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css':'source/styles/main.less'
+                }
+            }
+        }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production']);
 }
